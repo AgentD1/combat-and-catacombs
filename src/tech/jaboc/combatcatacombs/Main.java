dddf0046c0cb1912b3f3f0b08ec959da331a1fb3
AgentD1 <jamparker7@yahoo.com>
2019-06-23 16:30:16 -0400

    Added lame stuff
    Nothing important, just testing

--- src/tech/jaboc/combatcatacombs/Main.java
+++ src/tech/jaboc/combatcatacombs/Main.java
@@ -1,8 +1,20 @@
 package tech.jaboc.combatcatacombs;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    // write your code here
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("This is a test");
+		System.out.print("Enter your name: ");
+		String name = in.nextLine().toLowerCase();
+		if(name.equals("jacob")) {
+			System.out.println("Greetings Jacob, good to see you!");
+		} else if(name.equals("james")) {
+			System.out.println("Sup James, nice to see you!");
+		} else {
+			System.out.println("Hi, " + name);
+		}
+	}
 }
