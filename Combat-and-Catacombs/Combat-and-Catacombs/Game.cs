e17ea8944b84b86c126dbbe0005b1f4490c496d7
AgentD1 <jamparker7@yahoo.com>
2019-07-21 00:09:10 -0400

    Update Game.cs
    Credit of Shaydas1

--- Combat-and-Catacombs/Combat-and-Catacombs/Game.cs
+++ Combat-and-Catacombs/Combat-and-Catacombs/Game.cs
@@ -1,13 +1,27 @@
-﻿using System;
+using System;
 using System.Threading;
 
 namespace Combat_and_Catacombs {
     class Game {
         static void Main(string[] args) {
             bool quitting = false;
+            int[] playerpos = {4,4};
 
             while (!quitting) {
                 string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "N":
+                        if (playerpos[0] != 0)
+                        {
+                            playerpos[0] -= 1;
+                        }
+                    case "S":
+                        if (playerpos[0] != 9)
+                        {
+                            playerpos[0] += 1;
+                        }
+                }
                 quitting = CommandParser.Parse(input);
             }
             Console.WriteLine("Quitting...");
