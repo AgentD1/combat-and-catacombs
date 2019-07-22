84958f203903dce028f5632bc4d68dbd4d153428
Shaydas1 <jameswigg04@gmail.com>
2019-07-22 19:33:47 -0400

    Finished compass navigation

--- Combat-and-Catacombs/Combat-and-Catacombs/Game.cs
+++ Combat-and-Catacombs/Combat-and-Catacombs/Game.cs
@@ -5,9 +5,38 @@ namespace Combat_and_Catacombs {
     class Game {
         static void Main(string[] args) {
             bool quitting = false;
+            int[] playerpos = {4,4};
 
             while (!quitting) {
                 string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "N":
+                        if (playerpos[0] != 0)
+                        {
+                            playerpos[0] -= 1;
+                        }
+                        break;
+                    case "S":
+                        if (playerpos[0] != 9)
+                        {
+                            playerpos[0] += 1;
+                        }
+                        break;
+                    case "E":
+                        if (playerpos[1] != 9)
+                        {
+                            playerpos[1] += 1;
+                        }
+                        break;
+                    case "W":
+                        if (playerpos[1] != 0)
+                        {
+                            playerpos[1] -= 1;
+                        }
+                        break;
+                }
+                Console.WriteLine($"{playerpos[0]},{playerpos[1]}");
                 quitting = CommandParser.Parse(input);
             }
             Console.WriteLine("Quitting...");
