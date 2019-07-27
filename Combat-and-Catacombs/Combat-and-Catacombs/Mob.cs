03e3697bfc67d938e31d4d3a9ec2dc537005ee50
Shaydas1 <jameswigg04@gmail.com>
2019-07-27 16:09:24 -0400

    Added Mob.cs

---
+++ Combat-and-Catacombs/Combat-and-Catacombs/Mob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combat_and_Catacombs
+{
+    class Mob
+    {
+        public int health;
+
+        public Mob() {}
+    }
+}
