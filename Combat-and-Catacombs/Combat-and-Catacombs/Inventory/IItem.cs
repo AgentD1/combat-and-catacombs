b724ecde46b2d072a88ef2a9e6f51213204cf669
AgentD1 <jamparker7@yahoo.com>
2019-08-15 13:14:01 -0400

    Items and the like (WIP)

---
+++ Combat-and-Catacombs/Combat-and-Catacombs/Inventory/IItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combat_and_Catacombs {
+    public interface IItem {
+        string Name { get; }
+        float Weight { get; }
+        int MaxStackSize { get; }
+    }
+}
